bacd1d165818a63d1a937ff34171bb99c659448a
stephen johnson <github@sjoh.net>
2024-12-27 16:41:39 -0800

    Close pyaudio stream; write frames to file

--- get_audio.py
+++ get_audio.py
@@ -1,16 +1,19 @@
+import wave
+
 import pyaudio
 
 
 def main():
     fmt = pyaudio.paFloat32
     rate = 44100
+    channels = 1
     chunk = 1024
     record_seconds = 2
     frames = []
 
     audio = pyaudio.PyAudio()
 
-    in_dev = audio.open(format=fmt, channels=1, rate=rate, input=True, input_device_index=0, frames_per_buffer=chunk)
+    in_dev = audio.open(format=fmt, channels=channels, rate=rate, input=True, input_device_index=0, frames_per_buffer=chunk)
 
     print('Recording start')
     for i in range(0, int(rate / chunk * record_seconds)):
@@ -18,10 +21,17 @@ def main():
     print('Recording end')
 
     for frame in frames:
-        if(frame == 0):
-            print(frame)
-        else:
-            print('Frame is empty =(')
+        print(frame)
+    
+    in_dev.stop_stream()
+    in_dev.close()
+    audio.terminate()
+
+    with wave.open('testing.wav', 'wb') as f:
+        f.setnchannels(channels)
+        f.setsampwidth(audio.get_sample_size(fmt))
+        f.setframerate(rate)
+        f.writeframes(b''.join(frames))
 
 
 if __name__ == '__main__':
