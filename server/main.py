29e45ee88a16e9d711ea88f69e51e5c22ef0a797
Sam Burba <github@samburba.com>
2024-12-28 15:24:52 -0800

    Receive variable length messages on the server

--- server/main.py
+++ server/main.py
@@ -10,6 +10,7 @@ from pywhispercpp.model import Model
 MAX_COUNTER = 4_294_967_295
 FRAME_INDICATOR = 1
 END_INDICATOR = 2
+MAX_PACKET_SIZE = 65_507
 
 
 def write_audio(frames):
@@ -24,10 +25,8 @@ def write_audio(frames):
 def main():
     udp_ip = '0.0.0.0'
     udp_port = 8888
-    # TODO: Are frames always this big?
-    frame_len = 2048
-    packet_fmt = f'<hL{frame_len}h'
-    packet_size = struct.calcsize(packet_fmt)
+    header_fmt = f'<hLH'
+    header_size = struct.calcsize(header_fmt)
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.bind((udp_ip, udp_port))
 
@@ -38,14 +37,19 @@ def main():
     #    print(f'{i}: {segment.text}')
     last_frame_num = None
     while True:
-        data, _ = sock.recvfrom(packet_size)
-        indicator, frame_num, *frame = struct.unpack(packet_fmt, data)
+        data, _ = sock.recvfrom(MAX_PACKET_SIZE)
+        indicator, frame_num, audio_length = struct.unpack_from(header_fmt, data)
+        audio_fmt = f'<{audio_length}h'
+        print(f'indicator={indicator}, frame_num={frame_num}, audio_length={audio_length}, len={len(data)}')
+        audio_bytes = bytes(struct.unpack_from(audio_fmt, data, offset=header_size))
+        print(audio_bytes)
+
         print(f'Received frame: {frame_num}')
         if last_frame_num is not None:
             if frame_num != last_frame_num + 1 or (last_frame_num == MAX_COUNTER and frame_num != 0):
                 print('WARNING: Received frames out of order')
         if indicator == FRAME_INDICATOR:
-            frames.append(bytes(frame))
+            frames.append(audio_bytes)
         elif indicator == END_INDICATOR:
             print('End indicator recieved')
             #segments = model.transcribe(np.frombuffer(b''.join(frames), np.int16))
