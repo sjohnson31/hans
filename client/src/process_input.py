8a8274ca971ad6ec101df5c7aa20f93578a382e5
stephen johnson <github@sjoh.net>
2024-12-27 19:37:29 -0800

    Account for maximum 4 byte int

--- client/src/process_input.py
+++ client/src/process_input.py
@@ -3,6 +3,8 @@ import socket
 import struct
 from typing import NoReturn
 
+MAX_COUNTER = 4_294_967_295
+
 
 def send_audio_frames(frame_q: queue.Queue, server_sock: socket.socket, server_addr: str, server_port: int) -> NoReturn:
     frame_len = 0
@@ -15,4 +17,7 @@ def send_audio_frames(frame_q: queue.Queue, server_sock: socket.socket, server_a
             fmt = f'<L{frame_len}h'
         print('Got frame, sending')
         server_sock.sendto(struct.pack(fmt, frame_num, *frame), (server_addr, server_port))
-        frame_num += 1
+        if frame_num >= MAX_COUNTER:
+            frame_num = 0
+        else:
+            frame_num += 1
