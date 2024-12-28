0349ccaec0a1fe7b125a05a612727e7585d1744b
stephen johnson <github@sjoh.net>
2024-12-27 18:43:10 -0800

    Increment frame counter

--- client/src/process_input.py
+++ client/src/process_input.py
@@ -15,3 +15,4 @@ def send_audio_frames(frame_q: queue.Queue, server_sock: socket.socket, server_a
             fmt = f'<L{frame_len}h'
         print('Got frame, sending')
         server_sock.sendto(struct.pack(fmt, frame_num, *frame), (server_addr, server_port))
+        frame_num += 1
