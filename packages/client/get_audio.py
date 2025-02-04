b6a6d6311122bf59d3974b3a153467c788622dc2
Sam Burba <github@samburba.com>
2025-02-03 16:07:27 -0800

    Re-comment out audio config debug logs
    Created using spr 1.3.5

--- packages/client/get_audio.py
+++ packages/client/get_audio.py
@@ -29,8 +29,8 @@ def main():
     process_thread.start()
 
     audio = pyaudio.PyAudio()
-    for i in range(audio.get_device_count()):
-       print(audio.get_device_info_by_index(i))
+    # for i in range(audio.get_device_count()):
+    #    print(audio.get_device_info_by_index(i))
 
     def input_cb(in_data, frame_count: int, time_info, status_flags):
         out_data = None
