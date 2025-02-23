4c39658c52f8a80f5e2042d8cdf51aff64dce7d5
Sam Burba <github@samburba.com>
2025-02-22 17:29:38 -0800

    Use ruff to format and lint code
    Reviewers: sjohnson31
    
    Reviewed By: sjohnson31
    
    Pull Request: https://github.com/sjohnson31/hans/pull/11

--- packages/audio_debugger/audio_debugger.py
+++ packages/audio_debugger/audio_debugger.py
@@ -2,8 +2,11 @@ import sounddevice as sd
 import numpy as np
 from numpy._typing import DTypeLike
 
+
 class AudioDebugger:
-    def __init__(self, playback_seconds=5, sample_rate: int = 16_000, dtype: DTypeLike=np.int16):
+    def __init__(
+        self, playback_seconds=5, sample_rate: int = 16_000, dtype: DTypeLike = np.int16
+    ):
         self.playback_seconds = playback_seconds
         self.sample_rate = sample_rate
         # Coerce dtypelike to actual dtype
@@ -14,7 +17,7 @@ class AudioDebugger:
         self.audio_bytes.extend(audio_bytes)
         trigger_size = self.playback_seconds * self.sample_rate * self.dtype.itemsize
         if len(self.audio_bytes) > trigger_size:
-            print("Playing debug audio")
+            print('Playing debug audio')
             sd.play(
                 np.frombuffer(self.audio_bytes, self.dtype),
                 samplerate=self.sample_rate,
