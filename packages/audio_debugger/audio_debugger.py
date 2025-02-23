75621becc1f6a341f517134df32109ccc13b310a
Sam Burba <github@samburba.com>
2025-02-22 17:30:03 -0800

    Add save mode to audio debugger
    Reviewers: sjohnson31
    
    Reviewed By: sjohnson31
    
    Pull Request: https://github.com/sjohnson31/hans/pull/14

--- packages/audio_debugger/audio_debugger.py
+++ packages/audio_debugger/audio_debugger.py
@@ -1,26 +1,54 @@
+import enum
+import os
+from enum import Enum
 import sounddevice as sd
 import numpy as np
 from numpy._typing import DTypeLike
+import wave
+
+
+class DebugMode(Enum):
+    PLAYBACK = enum.auto()
+    SAVE = enum.auto()
 
 
 class AudioDebugger:
     def __init__(
-        self, playback_seconds=5, sample_rate: int = 16_000, dtype: DTypeLike = np.int16
+        self,
+        playback_seconds=5,
+        sample_rate: int = 16_000,
+        dtype: DTypeLike = np.int16,
+        mode=DebugMode.PLAYBACK,
     ):
         self.playback_seconds = playback_seconds
         self.sample_rate = sample_rate
         # Coerce dtypelike to actual dtype
         self.dtype = np.dtype(dtype)
         self.audio_bytes = bytearray()
+        self.mode = mode
+        self.debug_count = 0
 
     def append(self, audio_bytes: bytes | bytearray):
         self.audio_bytes.extend(audio_bytes)
         trigger_size = self.playback_seconds * self.sample_rate * self.dtype.itemsize
         if len(self.audio_bytes) > trigger_size:
-            print('Playing debug audio')
-            sd.play(
-                np.frombuffer(self.audio_bytes, self.dtype),
-                samplerate=self.sample_rate,
-                blocking=True,
-            )
-            self.audio_bytes = bytearray()
+            if self.mode == DebugMode.PLAYBACK:
+                print('Playing debug audio')
+                sd.play(
+                    np.frombuffer(self.audio_bytes, self.dtype),
+                    samplerate=self.sample_rate,
+                    blocking=True,
+                )
+                self.audio_bytes = bytearray()
+            else:
+                print('Saving debug audio')
+                os.makedirs('debug', exist_ok=True)
+                with wave.open(
+                    f'debug/debug_audio_{self.debug_count}.wav', 'wb'
+                ) as wav:
+                    wav.setnchannels(1)
+                    wav.setsampwidth(self.dtype.itemsize)
+                    wav.setframerate(self.sample_rate)
+                    wav.writeframes(self.audio_bytes)
+                self.debug_count += 1
+                self.audio_bytes = bytearray()
