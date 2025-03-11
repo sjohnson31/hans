dec278ede93cf3739ba177bc1be4ecc0b4ab6a6e
Sam Burba <github@samburba.com>
2025-03-10 17:01:50 -0700

    Move commands into their own classes
    Reviewers: sjohnson31
    
    Reviewed By: sjohnson31
    
    Pull Request: https://github.com/sjohnson31/hans/pull/28

--- packages/server/src/command_runner.py
+++ packages/server/src/command_runner.py
@@ -1,69 +1,38 @@
-from datetime import datetime
+import itertools
 import queue
-import threading
-import time
-from typing import Any
 
-from durations_nlp import Duration
-from durations_nlp.exceptions import ScaleFormatError
+from src.commands.command import Command
+from src.commands.timer_command import TimerCommand
+
+COMMANDS: list[Command] = [TimerCommand()]
+
+# In python 3.11, you cannot use backslashes in fstrings.
+# We can remove this once we migrate to 3.12 or higher
+_NEWLINE = '\n'
+GRAMMAR = f"""
+root ::= wake " " command
+wake ::= "Hey Hans,"
+command ::= ( {' | '.join([cmd.grammar_root for cmd in COMMANDS])} )
+{_NEWLINE.join([cmd.grammar for cmd in COMMANDS])}
+"""
+
+GRAMMAR_ROOT = 'root'
+
+# Transcription examples for expected voice commands
+# Used by whispercpp to guide transcription
+# https://github.com/ggerganov/whisper.cpp/blob/cadfc50eabb46829a0d5ac7ffcb3778ad60a1257/include/whisper.h#L516
+_ALL_TRAINING_SENTENCES = itertools.chain(
+    *[cmd.transcription_examples for cmd in COMMANDS]
+)
+INITIAL_PROMPT = '\n'.join(
+    [f'Hey Hans, {sentence}' for sentence in _ALL_TRAINING_SENTENCES]
+)
 
 
-def make_duration_string(duration: Duration) -> str:
-    if duration.to_minutes() < 1:
-        return f'{int(duration.to_seconds())} second'
-    minutes = int(duration.to_seconds() // 60)
-    seconds = int(duration.to_seconds() % 60)
-    return f'{minutes} minute and {seconds} second'
-
-
-def run_command(command_text: str, message_q: queue.Queue, sender_addr: Any) -> bool:
+def run_command(
+    command_text: str, message_q: queue.Queue, sender_addr: tuple[str, int]
+) -> None:
     command_text = command_text.removeprefix('Hey Hans, ')
-    return run_timer_command(command_text, message_q, sender_addr)
-
-
-def dumb_timer(seconds: int, message_q: queue.Queue, sender_addr: Any):
-    time.sleep(seconds)
-    message_q.put(('Your timer has expired', sender_addr))
-
-
-def run_timer_command(
-    command_text: str, message_q: queue.Queue, sender_addr: Any
-) -> bool:
-    if 'timer' not in command_text:
-        return False
-
-    duration_words = (
-        command_text.removeprefix('set a ')
-        .removeprefix('timer for ')
-        .removesuffix('.')
-        .removesuffix(' timer')
-    )
-
-    if 'and a half' in duration_words:
-        if 'hour' in duration_words:
-            addition = '30 minutes'
-        elif 'minute' in duration_words:
-            addition = '30 seconds'
-        elif 'second':
-            addition = '500 milliseconds'
-        else:
-            return False
-
-        duration_words = f'{duration_words.replace("and a half", "")} and {addition}'
-    try:
-        duration = Duration(duration_words)
-        print(f'Successfully found duration: {duration=}')
-    except ScaleFormatError:
-        print(f'Failed to get duration {duration_words}')
-        return False
-
-    dur_str = make_duration_string(duration)
-    print(f'Setting a timer for {dur_str} at {datetime.now()}')
-    message_q.put((f'Setting a {dur_str} timer', sender_addr))
-    t = threading.Thread(
-        target=dumb_timer,
-        args=[duration.to_seconds(), message_q, sender_addr],
-        daemon=True,
-    )
-    t.start()
-    return True
+    for command in COMMANDS:
+        if command.run(command_text, message_q, sender_addr):
+            break
