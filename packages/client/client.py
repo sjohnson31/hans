e5d6225ec8321916a42cae88c23e345d496c5975
stephen johnson <github@sjoh.net>
2025-03-12 19:38:20 -0700

    Clean up device getter

--- packages/client/client.py
+++ packages/client/client.py
@@ -19,12 +19,8 @@ def main():
     # Substring of the full device name given in sd.query_devices()
     # See https://python-sounddevice.readthedocs.io/en/0.5.1/api/streams.html#sounddevice.Stream
     # device parameter for more information
-    input_device = os.environ.get('INPUT_DEVICE')
-    output_device = os.environ.get('OUTPUT_DEVICE')
-    if not input_device:
-        input_device = sd.default.device[0]
-    if not output_device:
-        output_device = sd.default.device[1]
+    input_device = os.environ.get('INPUT_DEVICE', sd.default.device[0])
+    output_device = os.environ.get('OUTPUT_DEVICE', sd.default.device[1])
 
     process_thread = threading.Thread(
         target=stream_client_audio, args=(frame_q, server_ip, server_port), daemon=True
