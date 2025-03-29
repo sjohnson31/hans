96c48245698629efe3573651221a17cd77df8c00
Stephen Johnson <github@sjoh.net>
2025-03-29 14:30:16 -0700

    Add text file version of grocery command (#33)

---
+++ packages/server/src/commands/groceries/text_file_groceries_client.py
@@ -0,0 +1,13 @@
+import os
+
+from src.commands.groceries.groceries_client import GroceriesClient
+
+
+class TextFileGroceriesClient(GroceriesClient):
+    def __init__(self, file_path: os.PathLike):
+        self._file_path = file_path
+
+    def add_to_shopping_list(self, item: str) -> None:
+        with open(self._file_path, 'w+') as f:
+            f.write(f'{item}\n')
+        print(f'Wrote {item} to the grocery list')
